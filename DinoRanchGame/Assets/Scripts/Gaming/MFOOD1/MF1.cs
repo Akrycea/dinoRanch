c95d9125d17a3259d7c8f9b107344203e422d337
RakunTaperciak <maja.tapert@gmail.com>
2025-04-18 15:44:28 +0200

    pojawiają się jagódki :3
     zrobiłam tak że startujemy z pięcioma i co pare sekund się koleja pojawia, nwm czy tak zostawić czy po zebraniu żeby się nowy układ pojawił

--- DinoRanchGame/Assets/Scripts/Gaming/MFOOD1/MF1.cs
+++ DinoRanchGame/Assets/Scripts/Gaming/MFOOD1/MF1.cs
@@ -18,6 +18,7 @@ public class MF1 : MonoBehaviour
     public GameObject[] MinigameObjects;
     public int MF1berries_count;
     public TMP_Text MF1gameText;
+    public GameObject BerryPrefab;
 
     [SerializeField] private float gameTimer;
     private bool timeStart;
@@ -45,9 +46,12 @@ public class MF1 : MonoBehaviour
             //{
             //    MW1gameText.text = MW1_count + " berries";
            // }
+        
         }
     
     }
+    
+
     void MF1StartTimer()
     {
         gameTimer = gameTimer - Time.deltaTime;
@@ -63,7 +67,24 @@ public class MF1 : MonoBehaviour
         MF1berries_count=0;
         gameTimer=10;
         timeStart=true;
+        Invoke("SpawnFirstFiveBerries",0f);
+        InvokeRepeating("BerrySpawner",2f,2f);
     }
+
+    void SpawnFirstFiveBerries()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Vector3 randomSpawnPosition = new Vector3(UnityEngine.Random.Range(-2f,2f),UnityEngine.Random.Range(0f,3f), 0f);
+            Instantiate(BerryPrefab, randomSpawnPosition, Quaternion.identity);
+        }
+    }
+    void BerrySpawner()
+    {
+            Vector3 randomSpawnPosition = new Vector3(UnityEngine.Random.Range(-2f,2f),UnityEngine.Random.Range(0f,3f),0f);
+            Instantiate(BerryPrefab,randomSpawnPosition,Quaternion.identity);
+    }
+
     void MFood1Playing()
     {
         if (gameTimer < 0)
@@ -79,7 +100,16 @@ public class MF1 : MonoBehaviour
         RManager.FOOD= RManager.FOOD + MF1berries_count;
         StartCoroutine(waitingToEndGame());
         ClickManager.canClickBG=true;
-        
+        DestroyBerries();
+        CancelInvoke("BerrySpawner");
+    }
+    void DestroyBerries()
+    {
+        GameObject[] berries = GameObject.FindGameObjectsWithTag("Berry");
+        foreach (GameObject berry in berries)
+        {
+            Destroy(berry);
+        }
     }
     IEnumerator waitingToEndGame()
     {
