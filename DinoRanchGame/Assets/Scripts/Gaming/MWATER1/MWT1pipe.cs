45f97bee3f0095f1abdbcdc2d5c300085526d704
ratcase <150330908+ratcase@users.noreply.github.com>
2025-05-15 17:59:05 +0200

    pipesmmmmmmmm

--- DinoRanchGame/Assets/Scripts/Gaming/MWATER1/MWT1pipe.cs
+++ DinoRanchGame/Assets/Scripts/Gaming/MWATER1/MWT1pipe.cs
@@ -6,15 +6,37 @@ public class MWT1pipe : MonoBehaviour
 {
     float[] rotations ={ 0,90,180,270 };
 
+    public float correctRotation;
+    [SerializeField]
+    bool isPlaced = false;
+
     private void Start()
     {
+        correctRotation = transform.localEulerAngles.z;
         int rand = Random.Range(0, rotations.Length);   
         transform.eulerAngles = new Vector3(0, 0, rotations[rand]);
+
+        Check();
+
     }
 
     private void OnMouseDown()
     {
         transform.Rotate(new Vector3(0, 0, 90));
+
+        Check();
+    }
+
+    void Check()
+    {
+        if (transform.eulerAngles.z == correctRotation && isPlaced == false)
+        {
+            isPlaced = true;
+        }
+        else if (isPlaced == true)
+        {
+            isPlaced = false;
+        }
     }
 
 }
