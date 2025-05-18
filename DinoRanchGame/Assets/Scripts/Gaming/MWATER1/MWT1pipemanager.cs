b30637790e6b0d30520ce625860a5d934e59949f
ratcase <150330908+ratcase@users.noreply.github.com>
2025-05-18 21:45:35 +0200

    podpisalem kilka rzeczy w skrypcie

--- DinoRanchGame/Assets/Scripts/Gaming/MWATER1/MWT1pipemanager.cs
+++ DinoRanchGame/Assets/Scripts/Gaming/MWATER1/MWT1pipemanager.cs
@@ -15,17 +15,21 @@ public class MWT1pipemanager : MonoBehaviour
     
     void Start()
     {
-        solution = 0;
-        totalPipes = PipesHolder.transform.childCount;
+        //solution = 0;
 
+        //counts pipes (without the start and end ones)
+        totalPipes = PipesHolder.transform.childCount;
+        //holds all the pipes
         Pipes = new GameObject[totalPipes];
 
+        //puts all the pipes into the array
         for (int i = 0; i < Pipes.Length; i++)
         {
             Pipes[i] = PipesHolder.transform.GetChild(i).gameObject;
         }
     }
 
+    //this is supposed to be a win
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
